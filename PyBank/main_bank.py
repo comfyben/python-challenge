9bb7a11a134e69e06feee88f83e054e2d6e0f0e3
comfyben <bened.comfort@gmail.com>
2023-12-10 20:56:16 -0600

    finished

---
+++ PyBank/main_bank.py
@@ -0,0 +1,85 @@
+# modules
+import os
+import csv
+
+# set path
+bank_path = os.path.join("Resources", "budget_data.csv")
+
+print("FINANCIAL ANALYSIS")
+print("-------------------------")
+
+with open(bank_path) as bankfile:
+    bankreader = csv.reader(bankfile, delimiter=",")
+    bankheader = next(bankreader)
+
+    month_list = []
+    pl_list = []
+    change_list = []
+    month_count = 0
+    profit_total = 0
+    firstloop = True
+    prev_row = 1088983
+    for row in bankreader:
+        month_list.append(row[0])
+        pl_list.append(row[1])
+        month_count = month_count + 1
+        profit_total = profit_total + int(row[1])
+        if firstloop == True:
+            firstloop = False
+            profit_change = 0
+        elif int(row[1]) > 0:
+            if prev_row > 0:
+                profit_change = int(row[1]) - prev_row
+                prev_row = int(row[1])
+            elif prev_row < 0:
+                profit_change = int(row[1]) + (prev_row * -1)
+                prev_row = int(row[1])
+        elif int(row[1]) < 0:
+            if prev_row > 0:
+                profit_change = int(row[1]) +(prev_row * -1)
+                prev_row = int(row[1])
+            elif prev_row < 0:
+                profit_change = int(row[1]) - prev_row
+                prev_row = int(row[1])
+    
+        #print(profit_change)
+        change_list.append(profit_change)
+# print(change_list)
+change_total = sum(change_list)
+# print(change_total)
+
+        
+profit_avg = change_total/month_count
+max_increase = max(change_list)
+min_decrease = min(change_list)
+j = 0
+for i in change_list:
+    j = j + 1
+    if i == max_increase:
+        month_high = month_list[j - 1]
+        #print(month_list[j - 1])
+        #print(j)
+
+k = 0
+for l in change_list:
+    k = k + 1
+    if l == min_decrease:
+        month_low = month_list[k - 1]
+        #print(month_list[j - 1])
+        #print(j)
+print(f"Total Months: {month_count}")
+print(f"Total: ${profit_total}")
+print(f"Average Change: ${round(profit_avg, 2)}")
+print(f"Greatest Increase in Profits: {month_high} ({max_increase})" )
+print(f"Greatest Decrease in Profits: {month_low} ({min_decrease})")
+
+    # find the total number of months in the dataset
+
+    # the net total amount of "profits/losses"
+
+    # the changes in "profits/losses" over the entire period, then the average of those changes
+
+    # find the greatest increase in profits (date and amount)
+
+    # find the greatest increase in profits (date and amount)
+
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # python-challenge
-module3 python challenge homework
+# module3 python challenge homework
+
+# some resources here that I used to get through some road blocks
+
+# https://stackoverflow.com/questions/455612/limiting-floats-to-two-decimal-points
+# https://www.geeksforgeeks.org/python-program-to-find-largest-number-in-a-list/
+
+# please have terminal inside PyPoll or PyBank folder to run csv reader
+# I changed the names of the .py files so I wouldn't get confused
